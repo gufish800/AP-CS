22e6c8c5dc3c91df1723945d1c3b2efdaa3386bc
gufish800 <88512939+gufish800@users.noreply.github.com>
2021-08-15 19:24:33 -0400

    Update RandomRange.java

--- RandomRange.java
+++ RandomRange.java
@@ -1,8 +1,8 @@
 public class RandomRange {
-	public static int randomRange (int low, int high) {
-		double d = Math.random();
-		int range = high - low + 1;
-		int value =(int)(d*range)+low;
+	public static int randomRange (int low, int high) { 
+		double d = Math.random(); //picks between 0.0 to 0.1
+		int range = high - low + 1; //getting the amount of int
+		int value =(int)(d*range)+low; //d*range creates a random int. We then add low to make the number somewhere in the middle
 		return value;
 	}
 	
