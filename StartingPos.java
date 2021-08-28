8f457c7f9c4bbd5173915a6594ff63f4eebe1ac0
gufish800 <88512939+gufish800@users.noreply.github.com>
2021-08-27 22:57:43 -0400

    Add files via upload

---
+++ StartingPos.java
@@ -0,0 +1,49 @@
+
+public class StartingPos{
+	
+	public static int[] startingPostition(String primary,String pattern){
+		int [] arr = {0};
+		boolean found = false;
+		int nfind = 0;
+				
+		for (int i = 0; i < primary.length();i++){
+			String ss = primary.substring(i);
+			int index = ss.indexOf(pattern);
+
+			if (index == 0){
+	
+				found = true;
+				int[] oldArr = arr;
+				arr = new int[nfind + 1];
+				for(int j=0; j < oldArr.length; j++){ // copying old array to new array
+				
+					arr[j] = oldArr[j];
+			}
+			arr[nfind] = i;
+			nfind++;
+			
+				
+			}			
+			if (index == -1){
+				if (found == false){
+					System.out.println("-1");
+				}
+				break;			
+			}	
+		}
+		return arr;
+	}
+			
+	public static void main (String[] args){
+				
+		String primary = (args[0]);
+		String pattern = (args[1]);
+	    int[] indices = startingPostition(primary,pattern);
+		for (int i = 0; i< indices.length; i++){
+			System.out.println(indices[i]);	
+		}
+	
+	}
+	
+
+}
