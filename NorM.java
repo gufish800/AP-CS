f4d5e9eb6f3ff9324f80d186c8738fff3a5d1457
gufish800 <88512939+gufish800@users.noreply.github.com>
2021-08-27 22:13:34 -0400

    Update NorM.java

--- NorM.java
+++ NorM.java
@@ -1,16 +1,15 @@
 public class NorM {
 	public static void multiplesOfNorM (int m, int n) {
 		for(int k =1; k <= 1000; k++) {
-			if (k % n ==0 && k % m ==0){
-				//System.out.println("skip");
-				continue;
+			if (k % n ==0 && k % m ==0){ 
+				continue; //checks for both
 			}
 			
-			if (k % m == 0)
+			if (k % m == 0) //checks for one
 				System.out.println(k);
 			
 			
-			if (k % n ==0)
+			if (k % n ==0) //checks for the second
 				System.out.println(k);
 		}
 		
@@ -21,7 +20,5 @@ public class NorM {
 		int n = Integer.parseInt(args[1]);
 		
 		multiplesOfNorM(m,n);
-		
 	}
-	
 }
